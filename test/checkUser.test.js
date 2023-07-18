ff2cc008e5f3b737f3bd8614f6ff0a1e696ac8ea
Tsveta-Kamenova <walkerintherain@gmail.com>
2023-07-18 08:42:42 +0300

    flower ppersonal tests

--- test/checkUser.test.js
+++ test/checkUser.test.js
@@ -4,11 +4,18 @@ const chai = require('chai');
 
 const expect = chai.expect;
 
-describe('Check user', () => {
+describe.only('Check user', () => {
+  //describe.only('Check user', () => {
   it('When user is 85 years old it should be ADULT', () => {
     expect(checkUser(85), 'user when 85 years old').to.equal('ADULT');
   });
   it('When user is 5 years old it should be ADULT', () => {
     expect(checkUser(5), 'user when 5 years old').to.equal('NON-ADULT');
+  });
+  it('When user is 18 years old it should be ADULT', () => {
+    expect(checkUser(18), 'user when 18 years old').to.equal('NON-ADULT');
+  });
+  it('When user is 19 years old it should be ADULT', () => {
+    expect(checkUser(19), 'user when 19 years old').to.equal('ADULT');
   });
 });
--- test/findMax.test.js
+++ test/findMax.test.js
@@ -5,10 +5,14 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe.only('Check findMax1 function with three positive values', () => {
+  //describe.only('Check findMax1 function with three positive values', () => {
   it('when first is biggest', () => {
-    expect(findMax(10, 15, 20)).to.equal(20);
+    expect(findMax(15, 1, 2)).to.equal(15);
   });
   it('when second is biggest', () => {
-    expect(findMax(0, 0, 0)).to.equal(0);
+    expect(findMax(10, 55, 20)).to.equal(55);
+  });
+  it('when third is biggest', () => {
+    expect(findMax(10, 55, 200)).to.equal(200);
   });
 });
